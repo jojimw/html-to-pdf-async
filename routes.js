b3b92b6f6417e2e06d57d066ec98a468d2900466
Joji Mathew <jojimathew.mec@gmail.com>
2019-03-08 17:41:39 +0530

    download pdf added

--- routes.js
+++ routes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const generater = require('./utilities/generater');
+const logger = require('./utilities/initLogger');
+
 // respond with return data when post request is made
 router.post('/render', (req, res) => {
     dataObject = req.body;
@@ -11,13 +14,30 @@ router.post('/render', (req, res) => {
     res.send(responseDataObject);
 });
 
-router.post('/render-pdf', (req, res) => {
-    dataObject = req.body;
-    responseDataObject = require('./index')(req.body)
-    console.log("recieved request")
-    // let responseTemplate = JSON.stringify(responseDataObject.templateData);
-    // responseDataObject.templateData = responseTemplate;
-    res.send(responseDataObject);
+router.post('/download-pdf', (req, res) => {
+    // dataObject = req.body;
+    generater.renderPdfAndDownload(req.body)
+        .then(result => {
+            // logger.logDebug.debug('[from routes.js] post(/upload)\nResult:', result); 
+            res.send(result)
+        })
+        .catch(error => {
+            // logger.logError.error('[from routes.js] post(/upload)\n', error);
+            res.status(500).send(error);
+        })
+})
+
+router.post('/upload-pdf', (req, res) => {
+    // dataObject = req.body;
+    generater.renderPdfAndUpload(req.body)
+        .then(result => {
+            // logger.logDebug.debug('[from routes.js] post(/upload)\nResult:', result); 
+            res.send(result)
+        })
+        .catch(error => {
+            // logger.logError.error('[from routes.js] post(/upload)\n', error);
+            res.status(500).send(error);
+        })
 })
 
 module.exports = router;
--- utilities/generater.js
+++ utilities/generater.js
@@ -1,3 +1,25 @@
+var fs = require('fs');
+var pdf = require('html-pdf');
+var options = {
+    format: 'A4',
+    border: {
+        top: "0",  // default is 0, units: mm, cm, in, px
+        right: "2cm",
+        bottom: "0",
+        left: "2cm"
+    },
+    orientation: 'portrait',
+    header: {
+        height: '25mm'
+    },
+    footer: {
+        height: '25mm'
+    },
+    childProcessOptions: {
+        detached: true
+    }
+};
+ 
 const logger = require('./initLogger');
 const templateRegistry = require('./templateRegistry');
 const replaceStrings = require('./replaceString');
@@ -5,12 +27,12 @@ const replaceStrings = require('./replaceString');
 //get template from source 
 const getTemplate = (dataObject) => {
     try {
-        logger.logger_info.info('[From index.js] getTemplate() executed');
-        logger.logger_debug.debug('[From index.js] getTemplate() - type:', dataObject.type);
+        logger.logger_info.info('[From generater.js] getTemplate() executed');
+        logger.logger_debug.debug('[From generater.js] getTemplate() - type:', dataObject.type);
         return templateRegistry.fetchTemplate(dataObject);
     }
     catch (err) {
-        logger.logger_error.error('[From index.js] getTemplate() -', err, '\n');
+        logger.logger_error.error('[From generater.js] getTemplate() -', err, '\n');
         throw err;
     }
 }
@@ -18,34 +40,94 @@ const getTemplate = (dataObject) => {
 // function to generate html template using data object
 const generateTemplate = (dataObject) => {
     try {
-        logger.logger_info.info('[From index.js] generateTemplate() executed');
-        logger.logger_debug.debug('[From index.js] generateTemplate() executed');
-        logger.logger_access.info('[From index.js] generateTemplate() accessed');
+        logger.logger_info.info('[From generater.js] generateTemplate() executed');
+        logger.logger_debug.debug('[From generater.js] generateTemplate() executed');
+        logger.logger_access.info('[From generater.js] generateTemplate() accessed');
         let createTemplateResponse = replaceStrings(getTemplate(dataObject), dataObject);
         // return the template html string for the corresponding data object
         return createTemplateResponse;
     }
     catch (err) {
-        logger.logger_error.error('[From index.js] generateTemplate() -', err, '\n');
+        logger.logger_error.error('[From generater.js] generateTemplate() -', err, '\n');
         throw err;
     }
 }
+
 const appendToTemplate = (template, dataObject, appendData) => {
     try {
-        logger.logger_info.info('[From index.js] appendToTemplate() executed');
-        logger.logger_debug.debug('[From index.js] appendToTemplate() executed');        
-        logger.logger_access.info('[From index.js] appendToTemplate() accessed');
+        logger.logger_info.info('[From generater.js] appendToTemplate() executed');
+        logger.logger_debug.debug('[From generater.js] appendToTemplate() executed');        
+        logger.logger_access.info('[From generater.js] appendToTemplate() accessed');
         let createTemplateResponse = replaceStrings(template, dataObject, appendData);
         // return the template html string for the corresponding data object
         return createTemplateResponse;
     }
     catch (err) {
-        logger.logger_error.error('[From index.js] appendToTemplate() -', err, '\n');
+        logger.logger_error.error('[From generater.js] appendToTemplate() -', err, '\n');
         throw err;
     }
 };
 
+const renderPdfAndDownload = dataObject => {
+    try {
+        logger.logger_debug.debug('[From generater.js] renderPdfAndDownload executed');
+
+        if (dataObject.config.substitution) {
+            // generate template after substituting the variables
+            dataObject.templateData = generateTemplate(dataObject);
+            dataObject.templateData = dataObject.templateData.replace('\n', '');
+        }
+        if (dataObject.config.append) {
+            dataObject.templateData = appendToTemplate(dataObject.templateData, dataObject, dataObject.append);
+        }
+        return new Promise((resolve, reject) => {
+            pdf.create(dataObject.templateData, options).toFile('./out.pdf', (err, res) => {
+                if(err) {
+                    reject(err)
+                }
+                console.log('./out.pdf');
+                resolve({result: 'success'});
+            });
+        });
+    }
+    catch (err) {
+        logger.logger_error.error('[From generater.js] renderPdfAndDownload() - ', err, '\n');
+        throw err;
+    }
+}
+
+const renderPdfAndUpload = dataObject => {
+    try {
+        logger.logger_debug.debug('[From generater.js] renderPdfAndUpload executed');
+
+        if (dataObject.config.substitution) {
+            // generate template after substituting the variables
+            dataObject.templateData = generateTemplate(dataObject);
+            dataObject.templateData = dataObject.templateData.replace('\n', '');
+        }
+        if (dataObject.config.append) {
+            dataObject.templateData = appendToTemplate(dataObject.templateData, dataObject, dataObject.append);
+        }
+        return new Promise((resolve, reject) => {
+            pdf.create(dataObject.templateData, options).toStream(function(err, stream){
+                if(err) {
+                    reject(err)
+                }
+                console.log('./out.pdf');
+                resolve({result: 'success'});
+                stream.pipe(fs.createWriteStream('./foo.pdf'));
+            });
+        });
+    }
+    catch (err) {
+        logger.logger_error.error('[From generater.js] renderPdfAndUpload() - ', err, '\n');
+        throw err;
+    }
+}
+
 module.exports = {
-        generateTemplate,
-        appendToTemplate
+    generateTemplate,
+    appendToTemplate,
+    renderPdfAndUpload,
+    renderPdfAndDownload
 };
